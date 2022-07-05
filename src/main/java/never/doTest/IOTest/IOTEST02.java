c6072ef75e408bc9e3dcb7c96ca1ace5e68851c5
libospinach <45868766+libospinach@users.noreply.github.com>
2022-07-05 18:32:02 +0800

    git upload

--- src/main/java/never/doTest/IOTest/IOTEST02.java
+++ src/main/java/never/doTest/IOTest/IOTEST02.java
@@ -5,44 +5,44 @@ import java.io.*;
 /**
  * @Description: TODO
  * @author: Bo Li
- * @date: 2022年07月05日 14:42
+ * @date: 2022年07月05日 14:56
  */
-public class IOTest01 {
+public class IOTEST02 {
     public static void main(String[] args) {
         File f1 = new File("fileDirectory"+ File.separator+"test01.txt");
-        File f2 = new File("fileDirectory"+ File.separator+"writer01.txt");
+        File f2 = new File("fileDirectory"+ File.separator+"writer02.txt");
         FileReader fr = null;
         FileWriter fw = null;
         try {
             fr = new FileReader(f1);
-            fw = new FileWriter(f2,false);
+            fw = new FileWriter(f2);
+            char[] ch = new char[5];
             int n;
-            while((n=fr.read())!=-1){
-                fw.write(n);
+            while((n = fr.read(ch))!=-1){
+                fw.write(ch,0,n);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            try {
-                if(fw!=null){
-                    fw.close();
+        }finally{
+                try {
+                    if(fw!=null){
+                        fw.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
             try {
                 if(fr!=null){
                     fr.close();
                 }
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
+
     }
-
-
 }
--- src/main/java/never/doTest/IOTest/IOTest03.java
+++ src/main/java/never/doTest/IOTest/IOTest03.java
@@ -1,27 +1,31 @@
 package never.doTest.IOTest;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * @Description: TODO
  * @author: Bo Li
- * @date: 2022年07月05日 14:42
+ * @date: 2022年07月05日 18:21
  */
-public class IOTest01 {
+public class IOTest03 {
     public static void main(String[] args) {
         File f1 = new File("fileDirectory"+ File.separator+"test01.txt");
-        File f2 = new File("fileDirectory"+ File.separator+"writer01.txt");
+        File f2 = new File("fileDirectory"+ File.separator+"writer03.txt");
         FileReader fr = null;
         FileWriter fw = null;
         try {
             fr = new FileReader(f1);
-            fw = new FileWriter(f2,false);
-            int n;
-            while((n=fr.read())!=-1){
-                fw.write(n);
+            fw = new FileWriter(f2);
+            char[] ch = new char[6];
+            int n = fr.read(ch);
+            while(n!=-1){
+                String s = new String(ch,0,n);
+                fw.write(s);
+                n = fr.read(ch);
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }finally {
@@ -36,13 +40,9 @@ public class IOTest01 {
                 if(fr!=null){
                     fr.close();
                 }
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
     }
-
-
 }
