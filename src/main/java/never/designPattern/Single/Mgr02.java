1738b8503475e38dcb9ecccc75eeeb8b83fedfb7
30980 <309804010@qq.com>
2022-08-08 22:09:22 +0800

    degin pattern single 1

---
+++ src/main/java/never/designPattern/Single/Mgr02.java
@@ -0,0 +1,16 @@
+package never.designPattern.Single;
+
+public class Mgr02 {
+    private static final Mgr02 INSTANCE ;
+    static{
+        INSTANCE = new Mgr02();
+    }
+    private Mgr02(){}
+    public static Mgr02 getInstance(){
+        return INSTANCE;
+    }
+    public void m(){
+        System.out.println("单例模式1");
+    }
+
+}
